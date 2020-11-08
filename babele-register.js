f2e5e46af50b0dd17594b844409f91bf791d6ae0
CrimsonBL00D <74152006+CrimsonBL00D@users.noreply.github.com>
2020-11-08 19:31:31 +0100

    Add files via upload

---
+++ babele-register.js
@@ -0,0 +1,14 @@
+Hooks.once('init', () => {
+
+	if(typeof Babele !== 'undefined') {
+
+		Babele.get().register({
+			module: 'call-of-cthulhu-7th-babele-fr',
+			lang: 'fr',
+			dir: 'compendium'
+		});
+		
+		# document.getElementById("logo").src="/modules/call-of-cthulhu-7th-babele-es/img/fvtt-anvil-es.png";
+		
+	}
+});
